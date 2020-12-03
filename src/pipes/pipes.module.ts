2f160da356a017a6b2ab372a5f3faeea98fef917
ryosk7 <uchiryo7@gmail.com>
2020-12-04 06:36:28 +0900

    feat(pipe): HTMLタグ対応

---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { Nl2brPipe } from "./nl2br/nl2br";
+@NgModule({
+	declarations: [Nl2brPipe],
+	imports: [],
+	exports: [Nl2brPipe]
+})
+export class PipesModule {}
